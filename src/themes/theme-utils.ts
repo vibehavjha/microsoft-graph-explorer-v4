1916e0ad9da7cc9ff196a4c05554c62ccb4e3f0f
Elinor <ekaguongo@gmail.com>
2023-04-19 16:37:30 +0300

    Add variant service

--- src/themes/theme-utils.ts
+++ src/themes/theme-utils.ts
@@ -1,10 +1,22 @@
-const key = 'CURRENT_THEME';
+//const key = 'CURRENT_THEME';
 
 export function saveTheme(theme: string) {
-  localStorage.setItem(key, theme);
+  localStorage.setItem('CURRENT_THEME', theme);
 }
 
 export function readTheme() {
-  const theme = localStorage.getItem(key);
+  const theme = localStorage.getItem('CURRENT_THEME');
   return theme;
 }
+
+export const setToLS = (key: string, value: Object) => {
+  window.localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const getFromLS = (key: string) => {
+  const value = window.localStorage.getItem(key);
+
+  if (value) {
+    return JSON.parse(value);
+  }
+};
